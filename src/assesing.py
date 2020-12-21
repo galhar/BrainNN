261494c6cc1ab17ec4ae7493a818ff858ed19ff5
galhar <gal.harari11@gmail.com>
2020-12-21 14:42:55 +0200

    ready for fonts run with strong IINs and a little strong input (*3)

--- src/assesing.py
+++ src/assesing.py
@@ -237,7 +237,7 @@ def dummy_3d_func():
 
 if __name__ == '__main__':
     # average_one_checks(dummy_3d_func, runs=2)
-    load_path = '  records 2 4 5 6 8 9 10 11 12 13 14 12_21_20 13_34.json'
+    load_path = '  records                   12_21_20 13_54.json'
     check_func = output_distribution_query
-    setattr(check_func, 'title', 'output distribution Strong IINs (*10)')
+    setattr(check_func, 'title', 'classes acc OneOne eval, Strong IINs (*10)')
     average_over_nets(check_func, iterations=25, scatter=False, load=load_path)
