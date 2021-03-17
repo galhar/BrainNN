f813abe86d0e37a5fe22d4c3c9a6f11a7fb1edc6
galhar <gal.harari11@gmail.com>
2021-03-17 16:55:21 +0200

    changed rm_slurm

--- parallel_run/rm_slurm.sh
+++ parallel_run/rm_slurm.sh
@@ -1,1 +1,5 @@
 find . -name 'slurm*' -exec rm {} \;
+find . -name 'EvaluationHookData*' -exec rm {} \;
+find . -name 'collect_py_tmp*' -exec rm {} \;
+find . -name 'NetSavedByHook*' -exec rm {} \;
+rm -f tmp/*
